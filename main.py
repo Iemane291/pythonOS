2ec31cf25f6d17a263fc4964f7e42904614b5c95
lemane291 <xyyl691@gmail.com>
2022-02-24 10:45:51 +0400

    oh yeah i forgort this

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import time
 from datetime import datetime
 from colorama import Fore, Back, Style
 from pathlib import Path
+from platform import system as usersOS
 
 colorama.init(autoreset=True)
 
@@ -72,7 +73,7 @@ while True is not False:
             confirm = input().lower()
             if confirm == "y":
                 os.system("git pull origin main")
-                os.system("python3 main.py")
+                os.system("python3 main.py" if usersOS == "Darwin" else "py Main.py")
         elif msg == "exit":
             exit()
         elif msg == "memory":
