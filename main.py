75faa559b3249461887107bd88ce078c930f4697
Iemane291 <xyyl691@gmail.com>
2022-03-08 11:45:57 +0400

    oops

--- main.py
+++ main.py
@@ -250,7 +250,7 @@ while True is not False:
             from psutil import Process
 
             curMemory = Process(os.getpid())
-            print("Memory in megabytes: " + str(int(curMemory.memory_info().rss / 1000000)) + "MB")
+            print("Memory in megabytes: " + str(int(curMemory.memory_info().rss / 1e+6)) + "MB")
         
         elif msg.lower() == "pyedit":
             match usersOS():
